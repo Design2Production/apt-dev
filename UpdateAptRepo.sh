45d1a9149014d564ac133001399af2fd82e1d107
Joseph Narai <jan@d-p.com.au>
2022-07-26 21:31:25 -0400

    SK-197 Fixing Packages directory structure
    Resigning

--- UpdateAptRepo.sh
+++ UpdateAptRepo.sh
@@ -9,8 +9,14 @@ cp deb/*.deb docs/apt-repo/pool/main/.
 
 #mkdir -p apt-repo/dists/stable/main/binary-amd64
 
-dpkg-scanpackages --multiversion --arch amd64 docs/apt-repo/pool/ > docs/apt-repo/dists/stable/main/binary-amd64/Packages
+cd docs
 
-cat docs/apt-repo/dists/stable/main/binary-amd64/Packages | gzip -9 > docs/apt-repo/dists/stable/main/binary-amd64/Packages.gz
+dpkg-scanpackages --multiversion --arch amd64 apt-repo/pool/ > apt-repo/dists/stable/main/binary-amd64/Packages
 
-apt-ftparchive release docs/apt-repo/dists/stable/ > docs/apt-repo/dists/stable/Release
+cat apt-repo/dists/stable/main/binary-amd64/Packages | gzip -9 > apt-repo/dists/stable/main/binary-amd64/Packages.gz
+
+apt-ftparchive release apt-repo/dists/stable/ > apt-repo/dists/stable/Release
+
+cat apt-repo/dists/stable/Release | gpg --default-key design-to-production -abs --clearsign > apt-repo/dists/stable/InRelease
+
+cd ..
