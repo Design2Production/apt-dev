c7216fd2133e873428d1c6af7e781ad5b9e87c9d
Joseph Narai <jan@d-p.com.au>
2022-07-29 00:27:58 -0400

    DPEMS-97 readded main folder (doesn't seem to work without it)

--- update-apt-repo.sh
+++ update-apt-repo.sh
@@ -16,9 +16,9 @@ cp deb/$repoName/*.deb docs/apt-repo/pool/$repoName/.
 
 cd docs/apt-repo
 
-dpkg-scanpackages --multiversion --arch amd64 pool/$repoName > dists/$repoName/binary-amd64/Packages
+dpkg-scanpackages --multiversion --arch amd64 pool/$repoName > dists/$repoName/main/binary-amd64/Packages
 
-cat dists/$repoName/binary-amd64/Packages | gzip -9 > dists/$repoName/binary-amd64/Packages.gz
+cat dists/$repoName/binary-amd64/Packages | gzip -9 > dists/$repoName/main/binary-amd64/Packages.gz
 
 apt-ftparchive release dists/$repoName/ > dists/$repoName/Release
 
