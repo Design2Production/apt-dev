4a74c100e05b8c71041b1eb3c43cb0fd3670ee59
Joseph Narai <jan@d-p.com.au>
2022-08-10 16:03:37 +1000

    DPEMS096 Updating apt install files

--- setup-dp-apt-repo.sh
+++ setup-dp-apt-repo.sh
@@ -1,6 +1,24 @@
 #!/bin/bash
 #set -x #echo on
-wget -P /tmp -nc https://design2production.github.io/apt/dp-key.gpg
-install -o root -g root -m 644 /tmp/dp-key.gpg /etc/apt/trusted.gpg.d/
-wget -P /etc/apt/sources.list.d -nc https://design2production.github.io/apt/dp-apt-internal-stable.list
+
+clientName="$1"
+repoName="$2"
+
+if [ "$clientName" = "shopper-media" ] || [ "$clientName" = "internal" ] ; then
+   echo "Client           : $clientName"
+else
+   echo "Client must be specified: shopper-media | internal"
+   exit 1
+fi
+
+if [ "$repoName" = "stable" ] || [ "$repoName" = "testing" ] ; then
+   echo "Repo Name     : $repoName"
+else
+   echo "Repo Name must be specified: stable | testing"
+   exit 1
+fi
+
+url="https://design2production.github.io/apt/dp-apt-$clientName-$repoName.list"
+
+wget -P /etc/apt/sources.list.d -nc $url
 apt-get update
